10b0ca75c9b8636d79c923a74a799d7fcdb01fde
Mikael Ronström <mikael.ronstrom@gmail.com>
2023-09-20 00:50:41 +0200

    CLOUD-464: MySQL Server must start as daemon to interact with systemd

--- resources/rondb_scripts/mysql-server-start.sh
+++ resources/rondb_scripts/mysql-server-start.sh
@@ -43,7 +43,7 @@ if [ -n "$NO_DAEMON" ]; then
     echo "Starting the MySQL server as a foreground process"
     exec $mysqld_command
 else
-    $mysqld_command >/srv/hops/mysql-cluster/log/mysql_52_out.log 2>&1 </dev/null &
+    $mysqld_command --daemonize --log-error=/srv/hops/mysql-cluster/log/mysql_52_out.log --log-error-verbosity=3
 fi
 
 exit $?
